a41f0e571ff7a103e88aa634e571440f6fdb4739
=rfkih23 <=rifkihidayat23@yahoo.com>
2021-12-28 21:40:30 +0700

    register

--- src/components/Pages/Signup/Signup.js
+++ src/components/Pages/Signup/Signup.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+
 function Signup() {
     return (
-        <div>
-            Sign up login cokkkk
+        <div className='container'>
+            <div className='row'>
+                <div className='col-12 text-center'>
+                    <h1>SignUp Now!</h1>
+                    <p>
+                        Register now and start using this platform
+                    </p>
+                </div>
+            </div>
+            <div className='row mt-5'>
+                <div className='col-4'>
+                    <div className='card'>
+                        <div className='card-body'>
+                            <h4>
+                                Register
+                            </h4>
+                            <input
+                            name='fullName'
+                            placeholder='Full Name'
+                            type="text"
+                            ></input>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
