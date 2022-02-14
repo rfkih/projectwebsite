82a35479f41930986570cf87a78943ca90fc3aaf
=rfkih23 <=rifkihidayat23@yahoo.com>
2022-02-14 20:35:11 +0700

    no man

---
+++ latihan.js
@@ -0,0 +1,18 @@
+
+
+// class TaxCalculator {
+//     constructor() {
+
+//     }
+
+//     calculate(total) {
+//         return total * 0.05;
+//     }
+// }
+
+// var hitun = new TaxCalculator().calculate(50)
+
+// console.log(hitun);
+
+
+console.log(typeof(42));
--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -27,15 +27,16 @@ function Login() {
     onLogin()
   }
 
-  const onLogin =() =>{
-    axios.get("/users",{
+  const  onLogin = async () =>{
+    try {
+      const res = await axios.get("/users",{
         params:{username: formState.username, password: formState.password}
-    })
-    .then(res => {
-        const {id, username, role} =res.data[0]
+      })
+      const {id, username, role} =res.data[0]
         loginAction({dispatch, id, username, role})
-    })
-    .catch((err) => console.log({err}))
+    } catch (error) {
+      console.log({error})
+    }
 }
 
 
@@ -76,15 +77,8 @@ function Login() {
                   <Link to="/signup">Or Sign-Up</Link>
                   <button 
                     className="btn btn-primary mt-2"
-                    onClick={ onLoginClick }
-                  >
+                    onClick={ onLoginClick }>
                     Login
-                  </button>
-                  <button 
-                    className="btn btn-primary mt-2"
-                    onClick={event =>onLoginClick(event) }
-                  >
-                    Login 2
                   </button>
                 </div>
               </div>
